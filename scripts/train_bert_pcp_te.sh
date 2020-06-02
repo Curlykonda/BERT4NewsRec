217c9f424343c03543b21716f9f9b1efe7a7f3a4
HB <bartsch.henning@gmail.com>
2020-06-02 11:33:14 +0200

    Add 'SEED' to experiment descr

--- scripts/train_bert_pcp_te.sh
+++ scripts/train_bert_pcp_te.sh
@@ -44,7 +44,7 @@ do
   --temp_embs=$TE --incl_time_stamp=1 --temp_embs_hidden_units 256 $d_model --temp_embs_act_func $t_act_func \
   --max_article_len=$art_len --bert_feature_method $method $N \
   --lr $lr --decay_step $decay_step --cuda_launch_blocking=1 --device="cuda" \
-  --experiment_description $exp_descr $TE &
+  --experiment_description $exp_descr $TE s$SEED &
 
   python -u main.py --template train_bert_pcp --model_init_seed=$SEED \
   --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
@@ -52,7 +52,7 @@ do
   --num_epochs=50 --bert_num_blocks=1 --bert_max_len=50 \
   --max_article_len=$art_len --bert_feature_method $method $N \
   --lr $lr --decay_step $decay_step --cuda_launch_blocking=1 \
-  --experiment_description $exp_descr $TE &
+  --experiment_description $exp_descr $TE s$SEED &
 
   done
 done
