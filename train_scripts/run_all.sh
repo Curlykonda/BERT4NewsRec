9df9de4b0ebeff6eba4c78e03f895e15d09a53ae
HB <bartsch.henning@gmail.com>
2020-06-23 16:55:08 +0200

    Adjust 'log_period'

--- train_scripts/run_all.sh
+++ train_scripts/run_all.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-sbatch train_scripts/train_bert_npa_te.sh &
+sbatch -a 1-2 train_scripts/train_bert_npa_te.sh &
 
-sbatch train_scripts/train_bert_npa_pe.sh &
+sbatch -a 1-2 train_scripts/train_bert_npa_pe.sh &
+
+sbatch -a 1-2 train_scripts/train_bert_pe_medium.sh &
+sbatch -a 1-2 train_scripts/train_bert_pe_large.sh &
+
+sbatch -a 1-2 train_scripts/train_bert_te_medium.sh &
+sbatch -a 1-2 train_scripts/train_bert_te_large.sh &
+
+sbatch -a 1-2 train_scripts/train_npa &
+
+echo "all submitted"
