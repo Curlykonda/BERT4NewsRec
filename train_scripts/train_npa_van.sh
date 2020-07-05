76bd9014ac10f52087ca26cb91243dacecde4d66
HB <bartsch.henning@gmail.com>
2020-07-05 12:37:20 +0200

    update Lisa scripts

--- train_scripts/train_npa_van.sh
+++ train_scripts/train_npa_van.sh
@@ -5,7 +5,6 @@
 #SBATCH -p gpu_shared
 #SBATCH --mem=60000M
 
-
 module load pre2019
 module load Miniconda3/4.3.27
 source activate thesis-user-modelling
@@ -14,7 +13,7 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/medium_time_split_n_rnd_users/")
+data=("./Data/DPG_nov19/40k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 art_len=(30 128)
@@ -36,8 +35,7 @@ do
     #1
   python -u main.py --template train_npa --model_init_seed=$SEED --dataset_path=$data \
   --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-  --num_epochs=100 --max_article_len=$LEN \
-  --lr $lr --cuda_launch_blocking=1 \
+  --max_article_len=$LEN --lr $lr --cuda_launch_blocking=1 \
   --experiment_description $exp_descr l$LEN s$SEED
   ((COUNTER++))
   echo "$COUNTER"
