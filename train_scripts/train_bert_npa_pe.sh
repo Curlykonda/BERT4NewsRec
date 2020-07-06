01246553c97a1a74c000d345607288099586b61f
HB <bartsch.henning@gmail.com>
2020-07-06 11:34:56 +0200

    Try fix invalid value for 'n_users'

--- train_scripts/train_bert_npa_pe.sh
+++ train_scripts/train_bert_npa_pe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=npa_cnn_pe
 #SBATCH -n 8
-#SBATCH -t 28:00:00
-#SBATCH -p gpu_shared
+#SBATCH -t 1:00:00
+#SBATCH -p gpu_short
 #SBATCH --mem=60000M
 
 
@@ -24,11 +24,11 @@ neg_ratios=(4 24 49 99)
 
 enc="wucnn"
 d_art=400
+neg_sampler="random"
 
 nie="lin"
 #LR=(0.01, 0.001, 0.0001)
 lr=0.001
-#decay_step=25
 
 exp_descr="40k_NpaCNN"
 COUNTER=0
@@ -41,16 +41,18 @@ for POS in "${POS_EMBS[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    #1
-  python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
-  --train_negative_sampler_code random --train_negative_sample_size=$K \
-  --news_encoder $enc --dim_art_emb $d_art  --pt_word_emb_path=$w_emb --lower_case=1 \
-  --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie --n_users=$n_users \
-  --lr $lr --decay_step $decay_step --cuda_launch_blocking=1 \
-  --experiment_description $exp_descr $POS al$art_len k$K s$SEED
 
-  ((COUNTER++))
-  echo "Exp counter: $COUNTER"
+    echo "$exp_descr $TE al$art_len k$K s$SEED neg $neg_sampler"
+      #1
+    python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
+    --train_negative_sampler_code random --train_negative_sample_size=$K \
+    --news_encoder $enc --dim_art_emb $d_art  --pt_word_emb_path=$w_emb --lower_case=1 \
+    --pos_embs=$POS --max_article_len=$art_len --nie_layer=$nie --n_users=$n_users \
+    --lr $lr --cuda_launch_blocking=1 \
+    --experiment_description $exp_descr $POS al$art_len k$K s$SEED
+
+    ((COUNTER++))
+    echo "Exp counter: $COUNTER"
 
   done
 done
