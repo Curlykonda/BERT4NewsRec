006e0fcca1597ddf71fd8209367eb53047c4dae3
HB <bartsch.henning@gmail.com>
2020-07-06 11:59:19 +0200

    add echo statement for exp indicator

--- train_scripts/train_npa_mod.sh
+++ train_scripts/train_npa_mod.sh
@@ -5,7 +5,6 @@
 #SBATCH -p gpu_short
 #SBATCH --mem=60000M
 
-
 module load pre2019
 module load Miniconda3/4.3.27
 source activate thesis-user-modelling
@@ -24,7 +23,7 @@ d_art=400
 
 lr=0.001
 #decay_step=25
-
+n_users=40000
 exp_descr="npa_mod_40k"
 COUNTER=0
 
@@ -33,12 +32,14 @@ echo "$exp_descr $datapath"
 echo "$SEED"
 for LEN in "${art_len[@]}"
 do
+  echo "$exp_descr l$LEN s$SEED"
     #1
   python -u main.py --template train_mod_npa --model_init_seed=$SEED --dataset_path=$data \
   --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-  --max_article_len=$LEN --n_users 40000 \
+  --max_article_len=$LEN --n_users=$n_users \
   --lr $lr --cuda_launch_blocking=1 \
   --experiment_description $exp_descr l$LEN s$SEED
+
   ((COUNTER++))
   echo "$COUNTER"
 done
