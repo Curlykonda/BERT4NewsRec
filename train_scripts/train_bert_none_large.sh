000eb657a4b0994e483e5e48ff140140d2a2c414
HB <bartsch.henning@gmail.com>
2020-07-09 14:35:20 +0200

    Add script to test Bert4News w/o pos or temp embs

---
+++ train_scripts/train_bert_none_large.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=bertje_none_l
+#SBATCH -n 8
+#SBATCH -t 1:00:00
+#SBATCH -p gpu_short
+#SBATCH --mem=60000M
+
+
+module load pre2019
+module load Miniconda3/4.3.27
+source activate thesis-user-modelling
+
+python --version
+
+#srun -n 2 -t 00:30:00 --pty bash -il
+
+data=("./Data/DPG_nov19/100k_time_split_n_rnd_users/")
+pt_news_enc="BERTje"
+pt_news_enc_path="./BertModelsPT/bert-base-dutch-cased"
+
+SEED=$SLURM_ARRAY_TASK_ID
+
+TE=None
+
+art_len=30
+neg_ratios=(4 49)
+lr=0.001
+#decay_step=25
+
+
+nie="lin"
+d_model=768
+
+n_users=100000
+COUNTER=0
+#################
+
+exp_descr="100k_rnd_none"
+
+
+for K in "${neg_ratios[@]}"
+do
+  echo "$exp_descr $TE al$art_len k$K s$SEED"
+    #1
+  python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
+  --train_negative_sampler_code random --train_negative_sample_size=$K \
+  --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
+  --max_article_len=$art_len --nie_layer $nie --n_users=$n_users \
+  --lr $lr --cuda_launch_blocking=1 \
+  --experiment_description $exp_descr $TE al$art_len k$K s$SEED
+
+  ((COUNTER++))
+  echo "Exp counter: $COUNTER"
+done
