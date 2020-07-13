d52c1e2f464199db8e6153dc26cb91f7aaadacb5
HB <bartsch.henning@gmail.com>
2020-07-13 17:54:05 +0200

    Update scripts to test var 'hist_len'

--- train_scripts/bert_lpe_add_l.sh
+++ train_scripts/bert_lpe_add_l.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=bertje_pe_l
 #SBATCH -n 8
-#SBATCH -t 24:00:00
+#SBATCH -t 30:00:00
 #SBATCH -p gpu_shared
 #SBATCH --mem=60000M
 
@@ -22,6 +22,7 @@ pt_news_enc_path="./BertModelsPT/bert-base-dutch-cased"
 SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=30
+hist_len=50
 
 POS_EMBS=("lpe")
 neg_ratios=(4 9 24)
@@ -39,14 +40,15 @@ for K in "${neg_ratios[@]}"
 do
   for POS in "${POS_EMBS[@]}"
   do
-    echo "$exp_descr $POS al$art_len k$K s$SEED"
+    echo "$exp_descr $POS al$art_len hl$hist_len k$K s$SEED"
       #1
     python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
     --train_negative_sampler_code random --train_negative_sample_size=$K \
     --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
-    --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie \
+    --max_article_len=$art_len  --max_hist_len=$hist_len \
+    --pos_embs=$POS --nie_layer $nie \
     --lr $lr --n_users=$n_users --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr $POS al$art_len k$K s$SEED
+    --experiment_description $exp_descr $POS al$art_len hl$hist_len k$K s$SEED
 
     ((COUNTER++))
     echo "Exp counter: $COUNTER"
--- train_scripts/bert_tpe_add_l.sh
+++ train_scripts/bert_tpe_add_l.sh
@@ -19,9 +19,11 @@ data=("./Data/DPG_nov19/100k_time_split_n_rnd_users/")
 pt_news_enc="BERTje"
 pt_news_enc_path="./BertModelsPT/bert-base-dutch-cased"
 
-art_len=30
-#SEEDS=(113 42)
 SEED=$SLURM_ARRAY_TASK_ID
+
+art_len=30
+hist_len=50
+
 POS_EMBS=("tpe")
 neg_ratios=(4 9 24)
 
@@ -38,14 +40,15 @@ for K in "${neg_ratios[@]}"
 do
   for POS in "${POS_EMBS[@]}"
   do
-    echo "$exp_descr $POS al$art_len k$K s$SEED"
+    echo "$exp_descr $POS al$art_len hl$hist_len k$K s$SEED"
       #1
     python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
     --train_negative_sampler_code random --train_negative_sample_size=$K \
     --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
-    --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie \
+    --pos_embs=$POS --nie_layer $nie \
+    --max_article_len=$art_len --max_hist_len=$hist_len \
     --lr $lr --n_users=$n_users --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr $POS al$art_len k$K s$SEED
+    --experiment_description $exp_descr $POS al$art_len hl$hist_len k$K s$SEED
 
     ((COUNTER++))
     echo "Exp counter: $COUNTER"
