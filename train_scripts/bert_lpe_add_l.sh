29d46d9b1fdecb333cb5c3b593f0204afbe649ef
HB <bartsch.henning@gmail.com>
2020-07-18 15:38:23 +0200

    update scripts to run 'lr={1e-4; 1e-5}'

--- train_scripts/bert_lpe_add_l.sh
+++ train_scripts/bert_lpe_add_l.sh
@@ -14,7 +14,6 @@ python --version
 #srun -n 2 -t 00:30:00 --pty bash -il
 
 data=("./Data/DPG_nov19/100k_time_split_n_rnd_users/")
-#embeddings="../embeddings/cc.nl.300.bin"
 pt_news_enc="BERTje"
 pt_news_enc_path="./BertModelsPT/bert-base-dutch-cased"
 
@@ -27,7 +26,7 @@ POS_EMBS=("lpe")
 neg_ratios=(4) # 9
 
 nie="lin_gelu"
-lr=5e-4
+LR=(1e-4 1e-5)
 n_epochs=10
 
 n_users=100000
@@ -38,19 +37,22 @@ exp_descr="100k_add"
 
 for K in "${neg_ratios[@]}"
 do
-  for POS in "${POS_EMBS[@]}"
+  for lr in "${LR[@]}"
   do
-    echo "$exp_descr $POS al$art_len hl$hist_len k$K lr$lr s$SEED"
-      #1
-    python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
-    --train_negative_sampler_code random --train_negative_sample_size=$K \
-    --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
-    --max_article_len=$art_len  --max_hist_len=$hist_len \
-    --pos_embs=$POS --nie_layer $nie \
-    --lr $lr --n_users=$n_users --num_epochs=$n_epochs --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr $POS al$art_len k$K lr$lr s$SEED
+    for POS in "${POS_EMBS[@]}"
+    do
+      echo "$exp_descr $POS al$art_len hl$hist_len k$K lr$lr s$SEED"
+        #1
+      python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
+      --train_negative_sampler_code random --train_negative_sample_size=$K \
+      --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
+      --max_article_len=$art_len  --max_hist_len=$hist_len \
+      --pos_embs=$POS --nie_layer $nie \
+      --lr $lr --n_users=$n_users --num_epochs=$n_epochs --cuda_launch_blocking=1 \
+      --experiment_description $exp_descr $POS al$art_len k$K lr$lr s$SEED
 
-    ((COUNTER++))
-    echo "Exp counter: $COUNTER"
+      ((COUNTER++))
+      echo "Exp counter: $COUNTER"
+    done
   done
 done
--- train_scripts/bert_pe_cat_l.sh
+++ train_scripts/bert_pe_cat_l.sh
@@ -27,7 +27,7 @@ neg_ratios=(4)
 add_emb_size=512
 
 nie="lin_gelu"
-lr=1e-2
+LR=(1e-4 1e-5) # lr
 n_epochs=10
 
 n_users=100000
@@ -38,21 +38,24 @@ exp_descr="100k_cat"
 
 for K in "${neg_ratios[@]}"
 do
-  for POS in "${POS_EMBS[@]}"
+  for lr in "${LR[@]}"
   do
 
-    echo "$exp_descr $POS al$art_len k$K lr$lr s$SEED"
-      #1
-    python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
-    --train_negative_sampler_code random --train_negative_sample_size=$K \
-    --add_embs_func=concat --add_emb_size=$add_emb_size \
-    --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
-    --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie \
-    --lr $lr --num_epochs=$n_epochs --n_users=$n_users --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr $POS al$art_len k$K lr$lr LN s$SEED
+    for POS in "${POS_EMBS[@]}"
+    do
 
-    ((COUNTER++))
-    echo "Exp counter: $COUNTER"
+      echo "$exp_descr $POS al$art_len k$K lr$lr s$SEED"
+        #1
+      python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
+      --train_negative_sampler_code random --train_negative_sample_size=$K \
+      --add_embs_func=concat --add_emb_size=$add_emb_size \
+      --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
+      --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie \
+      --lr $lr --num_epochs=$n_epochs --n_users=$n_users --cuda_launch_blocking=1 \
+      --experiment_description $exp_descr $POS al$art_len k$K lr$lr LN s$SEED
+
+      ((COUNTER++))
+      echo "Exp counter: $COUNTER"
+    done
   done
-
 done
