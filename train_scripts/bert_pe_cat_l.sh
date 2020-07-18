ff54eb9e4e593d6a3f0a86c8514e817b359db2d3
HB <bartsch.henning@gmail.com>
2020-07-18 11:35:59 +0200

    update script with 'lr' as exponential notation

--- train_scripts/bert_pe_cat_l.sh
+++ train_scripts/bert_pe_cat_l.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=bertje_pe_l
 #SBATCH -n 8
-#SBATCH -t 10:00:00
-#SBATCH -p gpu_shared
+#SBATCH -t 1:00:00
+#SBATCH -p gpu_short
 #SBATCH --mem=60000M
-
 
 module load pre2019
 module load Miniconda3/4.3.27
@@ -15,7 +14,6 @@ python --version
 #srun -n 2 -t 00:30:00 --pty bash -il
 
 data=("./Data/DPG_nov19/100k_time_split_n_rnd_users/")
-#embeddings="../embeddings/cc.nl.300.bin"
 pt_news_enc="BERTje"
 pt_news_enc_path="./BertModelsPT/bert-base-dutch-cased"
 
@@ -29,7 +27,7 @@ neg_ratios=(4)
 add_emb_size=768
 
 nie="lin_gelu"
-lr=0.001
+lr=5e-4
 n_epochs=10
 
 n_users=100000
@@ -43,7 +41,7 @@ do
   for POS in "${POS_EMBS[@]}"
   do
 
-    echo "$exp_descr $POS al$art_len k$K LN s$SEED"
+    echo "$exp_descr $POS al$art_len k$K lr$lr LN s$SEED"
       #1
     python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
     --train_negative_sampler_code random --train_negative_sample_size=$K \
@@ -51,7 +49,7 @@ do
     --pt_news_enc=$pt_news_enc --path_pt_news_enc=$pt_news_enc_path \
     --pos_embs=$POS --max_article_len=$art_len --nie_layer $nie \
     --lr $lr --num_epochs=$n_epochs --n_users=$n_users --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr $POS al$art_len k$K LN s$SEED
+    --experiment_description $exp_descr $POS al$art_len k$K lr$lr LN s$SEED
 
     ((COUNTER++))
     echo "Exp counter: $COUNTER"
