2591e2ff992593de9bab03a7a65195b12c24d3b8
HB <bartsch.henning@gmail.com>
2020-07-22 13:30:33 +0200

    update scripts with var LR

--- train_scripts/bert_cnn_add_lpe_l.sh
+++ train_scripts/bert_cnn_add_lpe_l.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=npa_cnn_te
-#SBATCH -N 8
-#SBATCH -t 22:00:00
+#SBATCH --job-name=npa_cnn_lpe
+#SBATCH -N 4
+#SBATCH -t 30:00:00
 #SBATCH -p gpu_shared
 #SBATCH --mem=60G
 
@@ -20,10 +20,8 @@ SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=30
 
-TEMP_EMBS=("nte") # "lte"
-t_act_func="relu"
-
-neg_ratios=(4) # 9 24
+POS_EMBS=("lpe") #
+neg_ratios=(4) #
 
 enc="wucnn"
 d_art=400
@@ -38,26 +36,26 @@ n_users=100000
 exp_descr="100k_NpaCNN_add"
 COUNTER=0
 
-echo "$SLURM_JOBID"
-echo "$datapath"
-echo "$SEED"
+echo "$data"
+echo "$exp_descr"
 
+echo "$SEED"
 for K in "${neg_ratios[@]}"
 do
   for lr in "${LR[@]}"
   do
-    for TE in "${TEMP_EMBS[@]}"
+    for POS in "${POS_EMBS[@]}"
     do
-      echo "$exp_descr $TE al$art_len k$K lr$lr s$SEED" # nl$n_bert_layers
 
+      echo "$exp_descr $POS al$art_len k$K lr$lr s$SEED" # nl$n_bert_layers
+        #1
       python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
       --bert_num_blocks=$n_bert_layers --train_negative_sample_size=$K \
       --news_encoder $enc --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-      --temp_embs=$TE --incl_time_stamp=1 --add_embs_func=add \
-      --temp_embs_hidden_units 256 $d_art --temp_embs_act_func $t_act_func \
+      --pos_embs=$POS --add_embs_func=add \
       --max_article_len=$art_len --nie_layer=$nie --n_users=$n_users \
-      --lr=$lr --num_epochs=$n_epochs --cuda_launch_blocking=1 \
-      --experiment_description $exp_descr $TE al$art_len k$K lr$lr s$SEED
+      --lr $lr --num_epochs=$n_epochs --cuda_launch_blocking=1 \
+      --experiment_description $exp_descr $POS al$art_len k$K lr$lr s$SEED
 
       ((COUNTER++))
       echo "Exp counter: $COUNTER"
@@ -65,4 +63,3 @@ do
     done
   done
 done
-
