1bd85cbd68f1cbf2b65355311d38266956cd2f05
HB <bartsch.henning@gmail.com>
2020-07-25 13:56:28 +0200

    remove try except

--- source/evaluation/get_logs.py
+++ source/evaluation/get_logs.py
@@ -42,10 +42,10 @@ if __name__ == "__main__":
 
 
     for exp in exps:
-        try:
-            shutil.copytree(exp_dir.joinpath(exp + "/logs/"), log_dir.joinpath(exp))
-            if args.incl_model:
-                shutil.copytree(exp_dir.joinpath(exp + "/models/"), log_dir.joinpath(exp, "models"))
-        except:
-            pass
+        # try:
+        shutil.copytree(exp_dir.joinpath(exp + "/logs/"), log_dir.joinpath(exp))
+        if args.incl_model:
+            shutil.copytree(exp_dir.joinpath(exp + "/models/"), log_dir.joinpath(exp, "models"))
+        # except:
+        #     pass
 
