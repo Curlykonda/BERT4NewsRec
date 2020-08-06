62bf5c1561677022146b192c0bd95a523c2f70bb
HB <bartsch.henning@gmail.com>
2020-08-06 16:13:32 +0200

    add computation of individual user metrics

--- trainers/utils_metrics.py
+++ trainers/utils_metrics.py
@@ -24,7 +24,7 @@ def ndcg(scores, labels, k=10):
     ndcg = dcg / idcg
     return ndcg.mean()
 
-def mrr_score(scores, labels):
+def mrr_score(scores, labels, avg=True):
     """
     Calculate Mean Reciprocal Rank for a set of score-target pairs
 
@@ -35,8 +35,12 @@ def mrr_score(scores, labels):
     rank = scores.argsort(dim=1, descending=True)
     hits = labels.gather(1, rank)
     recpr_rank = hits / (torch.arange(hits.size(1)) + 1)
-    mrr = torch.sum(recpr_rank) / torch.sum(hits)
-    return mrr.item()
+    mrr = torch.sum(recpr_rank, dim=1) / torch.sum(hits, dim=1)
+
+    if avg:
+        return torch.mean(mrr).item()
+    else:
+        return list(mrr.cpu().numpy())
 
     # np variant:
     # order = np.argsort(y_score)[::-1] #Returns the indices that would sort an array
@@ -44,22 +48,24 @@ def mrr_score(scores, labels):
     # rr_score = y_true / (np.arange(len(y_true)) + 1)
     # return np.sum(rr_score) / np.sum(y_true)
 
-def calc_auc_and_mrr(scores, labels):
+def calc_auc_and_mrr(scores, labels, avg=True):
     scores = scores.cpu()
     labels = labels.cpu()
 
-    mrr = mrr_score(scores, labels)
-    auc = calc_roc_auc_tensor(scores, labels)
+    mrr = mrr_score(scores, labels, avg)
+    auc = calc_roc_auc_tensor(scores, labels, avg)
 
     return {"AUC": auc, "MRR": mrr}
 
-def calc_roc_auc_tensor(scores, labels):
+def calc_roc_auc_tensor(scores, labels, avg=True):
 
     auc = list(map(lambda y, x: roc_auc_score(y, x), labels, scores))
+    if avg:
+        return np.mean(auc)
+    else:
+        return auc
 
-    return np.mean(auc)
-
-def calc_recalls_and_ndcgs_for_ks(scores, labels, ks):
+def calc_recalls_and_ndcgs_for_ks(scores, labels, ks, avg=True):
     """
     scores:
     labels:
@@ -82,14 +88,25 @@ def calc_recalls_and_ndcgs_for_ks(scores, labels, ks):
 
             cut = cut[:, :k]
             hits = labels_float.gather(1, cut)
-            metrics['Recall@%d' % k] = \
-               (hits.sum(1) / torch.min(torch.Tensor([k]).to(labels.device), labels.sum(1).float())).mean().cpu().item()
+
+            recall = (hits.sum(1) / torch.min(torch.Tensor([k]).to(labels.device),
+                                         labels.sum(1).float()))
 
             position = torch.arange(2, 2+k)
             weights = 1 / torch.log2(position.float())
             dcg = (hits * weights.to(hits.device)).sum(1)
             idcg = torch.Tensor([weights[:min(int(n), k)].sum() for n in answer_count]).to(dcg.device)
-            ndcg = (dcg / idcg).mean()
-            metrics['NDCG@%d' % k] = ndcg.cpu().item()
+            ndcg = (dcg / idcg)
+
+            if avg:
+                recall = recall.mean().cpu().item()
+                ndcg = ndcg.mean().cpu().item()
+
+            else:
+                recall = list(recall.cpu().numpy())
+                ndcg = list(ndcg.cpu().numpy())
+
+            metrics['Recall@%d' % k] = recall
+            metrics['NDCG@%d' % k] = ndcg
 
     return metrics
