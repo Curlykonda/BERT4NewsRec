3d4d0c83c8a44fa75ba07e1d758a8040a614fa59
HB <bartsch.henning@gmail.com>
2020-09-07 12:47:55 +0200

    NPA baselines

--- train_scripts/npa_mod_cnn_m.sh
+++ train_scripts/npa_mod_cnn_m.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=m_npa_mod_cnn
-#SBATCH -n 8
-#SBATCH -t 20:00:00
+#SBATCH --job-name=npa_mod_cnn
+#SBATCH -n 4
+#SBATCH -t 35:00:00
 #SBATCH -p gpu_shared
-#SBATCH --mem=60000M
+#SBATCH --gres=gpu:2
+#SBATCH --mem=60G
 
 module load pre2019
 module load Miniconda3/4.3.27
@@ -13,39 +14,38 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/40k_time_split_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=(30)
-neg_ratios=(4 9 24)
+hist_len=100
+neg_ratios=(4 9) # 4
 
 d_art=400
 
-lr=0.001
+lr=1e-3
 epochs=100
-n_users=40000
+n_users=10000
 
-exp_descr="40k_npa_mod"
+exp_descr="10k_npa_mod"
 COUNTER=0
 #######
 
-echo "$exp_descr $datapath"
-
-echo "$SEED"
 for LEN in "${art_len[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    echo "$exp_descr l$LEN K$K s$SEED"
+    echo "$exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED"
       #1
-    python -u main.py --template train_mod_npa --model_init_seed=$SEED --dataset_path=$data \
-    --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-    --train_negative_sample_size=$K --max_article_len=$LEN \
-    --n_users=$n_users --num_epochs=$epochs \
-    --lr $lr --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr l$LEN k$K s$SEED
+    CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_mod_npa --model_init_seed=$SEED --dataset_path=$data \
+      --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
+      --train_negative_sample_size=$K --max_article_len=$LEN \
+      --max_hist_len=$hist_len --news_encoder=wucnn --npa_variant=custom \
+      --n_users=$n_users --num_epochs=$epochs \
+      --lr $lr --cuda_launch_blocking=1 \
+      --experiment_description $exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED
 
     ((COUNTER++))
     echo "$COUNTER"
--- train_scripts/npa_van_m.sh
+++ train_scripts/npa_van_m.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=npa_van
-#SBATCH -n 8
-#SBATCH -t 20:00:00
+#SBATCH -n 4
+#SBATCH -t 30:00:00
 #SBATCH -p gpu_shared
-#SBATCH --mem=60000M
+#SBATCH --gres=gpu:2
+#SBATCH --mem=60G
 
 module load pre2019
 module load Miniconda3/4.3.27
@@ -13,41 +14,39 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/40k_time_split_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
 
-art_len=(30 128)
-neg_ratios=(4 9 24)
+art_len=(30)
+hist_len=(100) # 50
+neg_ratios=(4 9) # 4 9
 
 d_art=400
 
 lr=0.001
-epochs=100
 
-n_users=40000
-exp_descr="40k_npa"
+n_users=10000
+exp_descr="10k_npa"
 COUNTER=0
+##################
 
-echo "$exp_descr $datapath"
-
-echo "$SEED"
-for LEN in "${art_len[@]}"
+for LEN in "${hist_len[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    echo "$exp_descr l$LEN k$K s$SEED"
+    echo "$exp_descr al$art_len hl$LEN k$K lr$lr s$SEED"
       #1
-    python -u main.py --template train_npa --model_init_seed=$SEED --dataset_path=$data \
-    --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-    --max_article_len=$LEN --n_users=$n_users --train_negative_sample_size=$K \
-    --lr $lr --cuda_launch_blocking=1 \
-    --experiment_description $exp_descr l$LEN k$K s$SEED
+    CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_npa --model_init_seed=$SEED --dataset_path=$data \
+      --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
+      --max_article_len=$art_len --max_hist_len=$LEN \
+      --n_users=$n_users --train_negative_sample_size=$K \
+      --lr $lr --cuda_launch_blocking=1 \
+      --experiment_description $exp_descr al$art_len hl$LEN k$K lr$lr s$SEED
     ((COUNTER++))
     echo "$COUNTER"
 
   done
 done
-
 
