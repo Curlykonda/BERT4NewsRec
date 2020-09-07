ed9ca9af5ea737d82d34e703df42aac753c93625
HB <bartsch.henning@gmail.com>
2020-09-07 12:45:42 +0200

    NPA baselines

--- train_scripts/npa_mod_cnn_l.sh
+++ train_scripts/npa_mod_cnn_l.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=npa_mod_cnn
 #SBATCH -n 4
-#SBATCH -t 10:00:00
+#SBATCH -t 35:00:00
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:2
 #SBATCH --mem=60G
@@ -21,26 +21,23 @@ SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=(30)
 hist_len=100
-neg_ratios=(4) # 4
+neg_ratios=(4 9) # 4
 
 d_art=400
 
-lr=1e-4
-epochs=50
+lr=1e-3
+epochs=100
 n_users=100000
 
 exp_descr="100k_npa_mod"
 COUNTER=0
 #######
 
-echo "$exp_descr $datapath"
-
-echo "$SEED"
 for LEN in "${art_len[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    echo "$exp_descr al$LEN hl$hist_len K$K s$SEED"
+    echo "$exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED"
       #1
     CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_mod_npa --model_init_seed=$SEED --dataset_path=$data \
       --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
@@ -48,7 +45,7 @@ do
       --max_hist_len=$hist_len --news_encoder=wucnn --npa_variant=custom \
       --n_users=$n_users --num_epochs=$epochs \
       --lr $lr --cuda_launch_blocking=1 \
-      --experiment_description $exp_descr al$LEN hl$hist_len k$K s$SEED
+      --experiment_description $exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED
 
     ((COUNTER++))
     echo "$COUNTER"
--- train_scripts/npa_van_l.sh
+++ train_scripts/npa_van_l.sh
@@ -20,7 +20,8 @@ w_emb="./pc_word_embeddings/cc.nl.300.bin"
 SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=(30)
-neg_ratios=(49 74 99) # 4 9
+hist_len=(100) # 50
+neg_ratios=(4 9) # 4 9
 
 d_art=400
 
@@ -29,20 +30,20 @@ lr=0.001
 n_users=100000
 exp_descr="100k_npa"
 COUNTER=0
-##################3
+##################
 
-echo "$SEED"
-for LEN in "${art_len[@]}"
+for LEN in "${hist_len[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    echo "$exp_descr l$LEN k$K s$SEED"
+    echo "$exp_descr al$art_len hl$LEN k$K lr$lr s$SEED"
       #1
     CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_npa --model_init_seed=$SEED --dataset_path=$data \
       --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
-      --max_article_len=$LEN --n_users=$n_users --train_negative_sample_size=$K \
+      --max_article_len=$art_len --max_hist_len=$LEN \
+      --n_users=$n_users --train_negative_sample_size=$K \
       --lr $lr --cuda_launch_blocking=1 \
-      --experiment_description $exp_descr l$LEN k$K s$SEED
+      --experiment_description $exp_descr al$art_len hl$LEN k$K lr$lr s$SEED
     ((COUNTER++))
     echo "$COUNTER"
 
