815c29014affee95c9dd62c2f159f92036419887
HB <bartsch.henning@gmail.com>
2020-09-17 13:07:33 +0200

    copy '/config.json' as well

--- source/evaluation/get_logs.py
+++ source/evaluation/get_logs.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     for exp in exps:
         try:
             shutil.copytree(exp_dir.joinpath(exp + "/logs/"), log_dir.joinpath(exp))
-            shutil.copytree(exp_dir.joinpath(exp + "config.json"), log_dir.joinpath(exp))
+            shutil.copytree(exp_dir.joinpath(exp + "/config.json"), log_dir.joinpath(exp))
             if args.incl_model:
                 shutil.copytree(exp_dir.joinpath(exp + "/models/"), log_dir.joinpath(exp, "models"))
             copied += 1
