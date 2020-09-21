8061407e72aa24587943b67e1f69fe9b1f4e1b01
HB <bartsch.henning@gmail.com>
2020-09-21 16:15:35 +0200

    min HL data for NPA

--- train_scripts/npa_mod_cnn_s.sh
+++ train_scripts/npa_mod_cnn_s.sh
@@ -14,14 +14,15 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
+#data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
 
 art_len=(30)
 hist_len=100
-neg_ratios=(4 9) # 4
+neg_ratios=(4) # 4
 
 d_art=400
 
@@ -30,6 +31,7 @@ epochs=200
 n_users=10000
 
 exp_descr="10k_npa_mod"
+add_info="min_hl50"
 COUNTER=0
 #######
 
@@ -37,15 +39,16 @@ for LEN in "${art_len[@]}"
 do
   for K in "${neg_ratios[@]}"
   do
-    echo "$exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED"
+    echo "$exp_descr $add_info al$LEN hl$hist_len k$K lr$lr s$SEED"
       #1
     CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_mod_npa --model_init_seed=$SEED --dataset_path=$data \
+      --dataset_add_info=$add_info \
       --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
       --train_negative_sample_size=$K --max_article_len=$LEN \
       --max_hist_len=$hist_len --news_encoder=wucnn --npa_variant=custom \
       --n_users=$n_users --num_epochs=$epochs \
       --lr $lr --cuda_launch_blocking=1 \
-      --experiment_description $exp_descr al$LEN hl$hist_len k$K lr$lr s$SEED
+      --experiment_description $exp_descr $add_info al$LEN hl$hist_len k$K lr$lr s$SEED
 
     ((COUNTER++))
     echo "$COUNTER"
