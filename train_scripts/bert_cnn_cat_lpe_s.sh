72a8f095bad86ab4774b82418314ddd28091c6ad
HB <bartsch.henning@gmail.com>
2020-09-21 12:25:36 +0200

    min HL data cat LPE

--- train_scripts/bert_cnn_cat_lpe_s.sh
+++ train_scripts/bert_cnn_cat_lpe_s.sh
@@ -14,7 +14,7 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
@@ -31,7 +31,6 @@ d_art=400
 
 n_layers=(2)
 n_heads=4
-#p_dropout=(0.1)
 p_d=0.2
 
 nie="lin_gelu"
@@ -39,7 +38,8 @@ lr=1e-3
 n_epochs=200
 
 n_users=10000
-exp_descr="10k_cnn_cat_shuffle_exc_t"
+exp_descr="10k_cnn_cat" # _shuffle_exc_t
+add_info="min_hl50"
 COUNTER=0
 ##########################
 
@@ -48,17 +48,16 @@ do
   for nl in "${n_layers[@]}"
   do
 
-    echo "$exp_descr $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED"
+    echo "$exp_descr $add_info $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED"
       #1
     CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
       --bert_num_blocks=$nl --bert_num_heads=$n_heads --bert_dropout=$p_d \
-      --train_negative_sampler_code random --train_negative_sample_size=$K \
+      --train_negative_sample_size=$K --dataset_add_info=$add_info \
       --pos_embs=$POS --add_embs_func=concat --add_emb_size=$add_emb_size \
-      --eval_seq_order='shuffle_exc_t'\
       --news_encoder $enc --dim_art_emb $d_art  --pt_word_emb_path=$w_emb --lower_case=1 \
       --max_article_len=$art_len --nie_layer=$nie --n_users=$n_users \
       --lr $lr --num_epochs=$n_epochs --cuda_launch_blocking=1 \
-      --experiment_description $exp_descr $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED
+      --experiment_description $exp_descr $add_info $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED
 
     ((COUNTER++))
     echo "Exp counter: $COUNTER"
