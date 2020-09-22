2c044735bae4ce12738881e358271f161862222f
HB <bartsch.henning@gmail.com>
2020-09-22 10:45:51 +0200

    shuffle normal data for LPE

--- train_scripts/bert_cnn_add_lpe_s.sh
+++ train_scripts/bert_cnn_add_lpe_s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=cnn_lpe_add
 #SBATCH -n 4
-#SBATCH -t 20:00:00
+#SBATCH -t 15:00:00
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:2
 #SBATCH --mem=60G
@@ -14,7 +14,8 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
+#data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
@@ -39,7 +40,7 @@ n_epochs=200
 
 n_users=10000
 exp_descr="10k_cnn_add_shuffle_exc_t"
-add_info="min_hl50"
+#add_info="min_hl50"
 COUNTER=0
 ####################################
 
@@ -51,17 +52,17 @@ do
   do
     for p_d in "${p_dropout[@]}"
     do
-      echo "$exp_descr $add_info $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED"
+      echo "$exp_descr $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED"
         #1
       CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
         --bert_num_blocks=$nl --bert_num_heads=$n_heads --bert_dropout=$p_d \
-        --eval_seq_order='shuffle_exc_t' --dataset_add_info=$add_info --train_negative_sample_size=$K  \
+        --eval_seq_order='shuffle_exc_t' --train_negative_sample_size=$K  \
         --pos_embs=$POS --add_embs_func=add \
         --news_encoder $enc --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
         --max_article_len=$art_len --max_hist_len=$hist_len \
         --nie_layer=$nie --n_users=$n_users \
         --lr $lr --num_epochs=$n_epochs --cuda_launch_blocking=1 \
-        --experiment_description $exp_descr $add_info $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED
+        --experiment_description $exp_descr $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d s$SEED
 
       ((COUNTER++))
       echo "Exp counter: $COUNTER"
