759a2802879abbc1dd0c56b1509b3c682dc42b59
HB <bartsch.henning@gmail.com>
2020-10-22 20:42:50 +0200

    none d_art=800

--- train_scripts/bert_cnn_none_s.sh
+++ train_scripts/bert_cnn_none_s.sh
@@ -14,8 +14,8 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
-#data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
+#data=("./Data/DPG_nov19/10k_min_hl50_n_rnd_users/")
+data=("./Data/DPG_nov19/10k_time_split_n_rnd_users/")
 w_emb="./pc_word_embeddings/cc.nl.300.bin"
 
 SEED=$SLURM_ARRAY_TASK_ID
@@ -27,7 +27,7 @@ POS=None #
 neg_ratios=(4) # 24
 
 enc="wucnn"
-d_art=400
+d_art=800
 
 n_layers=(2)
 HEADS=(4)
@@ -40,7 +40,7 @@ n_epochs=200
 
 n_users=10000
 exp_descr="10k_cnn" # _brand_s
-add_info="min_hl50"
+#add_info="min_hl50" --dataset_add_info=$add_info
 
 COUNTER=0
 #############################
@@ -51,16 +51,16 @@ do
   do
     for n_heads in "${HEADS[@]}"
     do
-      echo "$exp_descr $add_info $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d pM$p_m s$SEED"
+      echo "$exp_descr $POS al$art_len hl$hist_len k$K lr$lr L$nl H$n_heads pD$p_d pM$p_m s$SEED"
         #1
       CUDA_VISIBLE_DEVICES=0,1 python -u main.py --template train_bert_pcp --model_init_seed=$SEED --dataset_path=$data \
         --bert_num_blocks=$nl --bert_num_heads=$n_heads --bert_dropout=$p_d --bert_mask_prob=$p_m \
-        --train_negative_sample_size=$K --dataset_add_info=$add_info \
+        --train_negative_sample_size=$K \
         --news_encoder $enc --dim_art_emb $d_art --pt_word_emb_path=$w_emb --lower_case=1 \
         --max_article_len=$art_len --max_hist_len=$hist_len \
         --nie_layer=$nie --n_users=$n_users \
         --lr $lr --num_epochs=$n_epochs --cuda_launch_blocking=1 \
-        --experiment_description $exp_descr $add_info $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d pM$p_m s$SEED
+        --experiment_description $exp_descr $POS al$art_len k$K lr$lr L$nl H$n_heads pD$p_d pM$p_m s$SEED
 
       ((COUNTER++))
       echo "Exp counter: $COUNTER"
